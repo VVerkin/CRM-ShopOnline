02751c0ef33e5a9c028b2648d6c7882484d2ea47
Василий Веркин <v.verkin@gmail.com>
2023-06-24 16:50:10 +0300

    Стоимость товара

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-alert('Hello JavaScript');
+'use strict';
+
+const goodsName = 'Телевизор Soni';
+const goodsCount = 3;
+const goodsCategory = 'Техника для дома';
+const goodsPrice = 100;
+const goodsCurrency ='$'
+const goodsResult = goodsCount * goodsPrice;
+
+console.log (goodsResult + goodsCurrency);
+
+

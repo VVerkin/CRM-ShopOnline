879ba0cfbeaaf4e62c511565254bdf68a6016479
Vasilii Verkin <v.verkin@gmail.com>
2023-11-10 23:22:34 +0300

    добавил id и кнопку в шапку модального окна

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,19 @@
 'use strict'
+
+// Заголовок модального окна
+const modalTitle = document.querySelector('.modal__title');
+console.log(modalTitle);
+
+// Кнопка возле id и сам id
+
+// Форма
+const form = document.querySelector('.modal__form');
+console.log(form);
+
+// Чекбокс и  Поле рядом с чекбоксом
+const discountCheckbox = document.querySelector('.modal__form-checkbox-input');
+const discountInput = document.querySelector('.modal__form-input-small');
+console.log(discountCheckbox, discountInput );
+// Итоговую стоимость
+const resultCoast = document.querySelector('.modal__goods-value');
+console.log(resultCoast);

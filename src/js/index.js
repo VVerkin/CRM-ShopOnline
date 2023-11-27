dd05724137b7b5273b228b208d3aee7949cc60c7
Vasilii Verkin <v.verkin@gmail.com>
2023-11-27 23:29:10 +0300

    активировал модальное окно

--- src/js/index.js
+++ src/js/index.js
@@ -5,10 +5,10 @@ const modalTitle = document.querySelector('.modal__title');
 console.log(modalTitle);
 
 // Кнопка возле id и сам id
-
 const goodsId = document.querySelector('.modal__id-value');
 const editGoodsIdBtn = document.querySelector('.madal__id-btn');
 console.log(goodsId, editGoodsIdBtn);
+
 // Форма
 const form = document.querySelector('.modal__form');
 console.log(form);
@@ -17,6 +17,7 @@ console.log(form);
 const discountCheckbox = document.querySelector('.modal__form-checkbox-input');
 const discountInput = document.querySelector('.modal__form-input-small');
 console.log(discountCheckbox, discountInput );
-// Итоговую стоимость
+
+// Итоговая стоимость
 const resultCoast = document.querySelector('.modal__goods-value');
 console.log(resultCoast);
